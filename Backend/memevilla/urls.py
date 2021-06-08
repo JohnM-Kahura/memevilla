60c5de8c5455a3860ec9718fe805726c2b458c0f
JohnM-Kahura <johnmungai392@gmail.com>
2021-06-08 15:47:07 +0300

    settings changed

--- Backend/memevilla/urls.py
+++ Backend/memevilla/urls.py
@@ -4,6 +4,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/',include('allauth.urls')),
     path('verification/',include('verify_email.urls')),
     path('',include('post.urls'),name='posts'),
     path('api/',include('meme_api.urls'),name='meme_api'),
