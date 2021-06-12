547d07b250f0c59442ee7a61377793ac11cbe67b
JohnM-Kahura <johnmungai392@gmail.com>
2021-06-12 10:55:20 +0300

    fixed import error

--- Backend/post/models.py
+++ Backend/post/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from ..user.models import CustomUser 
+from user.models import CustomUser 
 #TODO: add translation in the future use gettext_lazy func
 class Post(models.Model):
     image=models.CharField(max_length=5000)
@@ -7,7 +7,7 @@ class Post(models.Model):
     views=models.IntegerField(default=0)
     date_time=models.DateTimeField( auto_now=True)
     comments=models.ForeignKey("Comment",on_delete=models.SET_NULL,null=True,blank=True)
-    user=models.ForeignKey("CustomUser",on_delete=models.CASCADE)
+    user=models.ForeignKey(CustomUser,on_delete=models.CASCADE,null=False,blank=False)
     objects=models.Manager()#default manager
     def __str__(self):
         return self.image   
