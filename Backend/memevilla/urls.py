9185877d85ddd7a6f128786ac7e6a52539f556a9
JohnM-Kahura <johnmungai392@gmail.com>
2021-06-22 11:23:12 +0300

    changes made

--- Backend/memevilla/urls.py
+++ Backend/memevilla/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('verification/',include('verify_email.urls')),
     path('',include('post.urls'),name='posts'),
     path('api/',include('meme_api.urls'),name='meme_api'),
+    path('api/user/',include('user.urls',namespace='users')),
     path('api-auth/',include('rest_framework.urls',namespace='rest_framework')),
     path('api/token/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
     path('api/token/refresh',TokenRefreshView.as_view(),name='token_refresh'),
+
 ]
